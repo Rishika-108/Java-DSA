3840b9b22dfea96b5b04644ac41d50c3bd3f56b0
Rishika Thakur <159441865+Rishika-108@users.noreply.github.com>
2026-05-30 14:33:45 +0530

    Time: 0 ms (100%), Space: 47.6 MB (78.26%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+
+        double half = myPow(x, n/2);
+        if( n % 2 == 0) return half * half;
+        else {
+           if(n < 0) return (1.0/x) * half * half;
+           else return x * half * half;   
+        } 
+    }
+}
